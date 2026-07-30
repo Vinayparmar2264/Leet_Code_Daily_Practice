32c9bbe493c2693db24c0786e123ff30b237bbb9
Vinay Parmar <vinayparmar2264@gmail.com>
2026-07-30 19:48:40 +0530

    Time: 8 ms (30.75%), Space: 21.9 MB (78.74%) - LeetHub

--- 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.py
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.py
@@ -4,27 +4,45 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+class BSTIterator:
+    def __init__(self,root,reverse):
+        self.stack = []
+        self.reverse = reverse
+        self.pushAll(root)
+
+    def pushAll(self,node):
+        while node:
+            self.stack.append(node)
+            if self.reverse:
+                node = node.right
+            else:
+                node = node.left
+    
+    def next(self):
+        node = self.stack.pop()
+        if self.reverse:
+            self.pushAll(node.left)
+        else:
+            self.pushAll(node.right)
+        
+        return node.val
+
+
 class Solution:
     def findTarget(self, root: Optional[TreeNode], k: int) -> bool:
-        def inorder(node):
-            if node is None:
-                return
-            
-            inorder(node.left)
-            result.append(node.val)
-            inorder(node.right)
-        
-        result = []
-        inorder(root)
+        left = BSTIterator(root,False)
+        right = BSTIterator(root,True)
 
-        i = 0
-        j = len(result)-1
+        i = left.next()
+        j = right.next()
 
-        while i<j:
-            if result[i] + result[j] == k:
+        while i < j:
+            s = i + j
+            if s == k :
                 return True
-            elif result[i] + result[j] > k:
-                j -= 1
+            elif s < k :
+                i  = left.next()
             else:
-                i += 1
+                j = right.next()
         return False
