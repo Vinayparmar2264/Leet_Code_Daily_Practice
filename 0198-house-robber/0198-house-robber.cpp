8005d1e09981dedb60f6b356688f6e00b60704bc
Vinay Parmar <vinayparmar2264@gmail.com>
2026-08-16 00:38:12 +0530

    Time: 0 ms (100%), Space: 10 MB (90.29%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.cpp
@@ -0,0 +1,29 @@
+
+class Solution {
+public:
+    int n;
+    int dp[101];
+    int solve(vector<int>&nums , int idx)
+    {
+        if(idx<0) return 0;
+
+        if(dp[idx] != -1) return dp[idx];
+
+        int take=nums[idx]+solve(nums,idx-2);
+        int skip=solve(nums,idx-1);
+
+        return dp[idx]=max(take,skip);
+    }
+    int rob(vector<int>& nums) {
+        
+        n=nums.size();
+        
+        if(n==1) return nums[0];
+        
+        memset(dp,-1,sizeof(dp));
+
+        return solve(nums,n-1);
+        
+        // return max(dp[n-1] ,dp[n-2]);
+    }
+};
