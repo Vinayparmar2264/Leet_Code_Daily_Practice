1735154b410bbeaadeea4b75e809c3441ab0a305
Vinay Parmar <vinayparmar2264@gmail.com>
2026-08-19 01:05:39 +0530

    Time: 747 ms (12.11%), Space: 21.6 MB (27.28%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.py
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.py
@@ -1,17 +1,19 @@
 class Solution:
-    def solve(self,i,j,s,dp):
-        if i>j:
+    def solve(self,i,j,s1,s2,dp):
+        if i < 0 or j < 0 :
             return 0
-        if dp[i][j]!=-1:
+        if dp[i][j] != -1:
             return dp[i][j]
-        if s[i] == s[j]:
-            dp[i][j] = self.solve(i+1,j-1,s,dp)
+        
+        if s1[i] == s2[j]:
+            dp[i][j] =  1 + self.solve(i-1,j-1,s1,s2,dp)
             return dp[i][j]
-        dp[i][j] = 1 + min (self.solve(i+1,j,s,dp),self.solve(i,j-1,s,dp))
+        dp[i][j] = max(self.solve(i-1,j,s1,s2,dp),self.solve(i,j-1,s1,s2,dp))
         return dp[i][j]
-
     def minInsertions(self, s: str) -> int:
         n = len(s)
         dp = [[-1]*(n+1) for _ in range(n+1)]
-        return self.solve(0,len(s)-1,s,dp)
-        
+        s2 = s[::-1]
+        common_sequence =  self.solve(n-1,n-1,s,s2,dp)
+        required = n - common_sequence
+        return required
