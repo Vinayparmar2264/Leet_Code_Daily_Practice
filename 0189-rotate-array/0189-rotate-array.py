72037209104abed2507e120352ef1fedd537f223
Vinay Parmar <vinayparmar2264@gmail.com>
2026-08-19 01:17:10 +0530

    Time: 3 ms (75.66%), Space: 24.4 MB (100%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -32,7 +32,8 @@ class Solution:
             nums[left],nums[right] = nums[right],nums[left]
             left +=  1
             right -= 1
- 
+
+        return nums    
             
 
             
