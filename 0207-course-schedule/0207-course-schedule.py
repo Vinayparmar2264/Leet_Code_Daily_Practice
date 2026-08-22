813decf50a6f056225e8a2fed238329330a5b585
Vinay Parmar <vinayparmar2264@gmail.com>
2026-08-22 14:22:26 +0530

    Time: 0 ms (100%), Space: 20.6 MB (53.27%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,26 +1,32 @@
-from collections import deque
 class Solution:
+    def dfs(self,node,adj,visit ,path_visit):
+        visit[node] = 1
+        path_visit[node] = 1
+    
+        for adjNode in adj[node]:
+            if visit[adjNode] == 0:
+               ans =  self.dfs(adjNode,adj,visit,path_visit)
+               if ans == False:
+                    return False
+            elif path_visit[adjNode] == 1:
+                return False
+        
+        path_visit[node] = 0
+        return True
+            
+
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         adj = [[] for _ in range(numCourses)]
-        indegree = [0 for _ in range(numCourses)]
+
         for u,v in prerequisites:
             adj[v].append(u)
-            indegree[u] += 1
-        
-        que = deque([])
-        result = []
 
-        for i in range(numCourses):
-            if indegree[i] == 0:
-                que.append(i)
+        visit = [0 for _ in range(numCourses)]
+        path_visit = [0 for _ in range(numCourses)]
         
-        while que :
-            node  = que.popleft()
-            result.append(node)
-            for adjNode in adj[node]:
-                indegree[adjNode] -= 1
-                if indegree[adjNode] == 0:
-                    que.append(adjNode)
-        if len(result) == numCourses:
-            return True
-        return False
+        for i in range(numCourses):
+            if visit[i] == 0:
+                if  self.dfs(i,adj,visit,path_visit) == False:
+                    return False
+        
+        return True
