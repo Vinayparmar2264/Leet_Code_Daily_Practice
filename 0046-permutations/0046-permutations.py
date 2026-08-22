dd93b6e13539eda6aac9a009e1077509ab07efdd
Vinay Parmar <vinayparmar2264@gmail.com>
2026-08-23 02:15:33 +0530

    Time: 3 ms (31.46%), Space: 19.4 MB (87.74%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,20 +1,18 @@
 class Solution:
-    def solve(self,nums,temp,st,result):
-        if len(temp) == len(nums):
-            result.append(list(temp))
+    def solve(self,idx,nums,result):
+        if idx >= len(nums):
+            result.append(nums[:])
             return
         
-        for i in range(len(nums)):
-            if  nums[i] not in st:
-                temp.append(nums[i])
-                st.add(nums[i])
-                self.solve(nums,temp,st,result)
-                temp.pop()
-                st.remove(nums[i])
+        for i in range(idx,len(nums)):
+            nums[idx],nums[i] = nums[i],nums[idx]
+            self.solve(idx+1,nums,result)
+            nums[idx],nums[i] = nums[i],nums[idx]
+   
 
         
     def permute(self, nums: List[int]) -> List[List[int]]:
-        st = set()
+    
         result = []
-        self.solve(nums,[],st,result)
+        self.solve(0,nums,result)
         return result
