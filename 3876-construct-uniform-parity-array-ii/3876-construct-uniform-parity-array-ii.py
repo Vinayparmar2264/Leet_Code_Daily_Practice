8b48ac437e90e2ac13ddc8804e0b19ec24935a4c
Vinay Parmar <vinayparmar2264@gmail.com>
2026-09-05 17:07:36 +0530

    Time: 62 ms (44.03%), Space: 35.4 MB (39.1%) - LeetHub

--- 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.py
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.py
@@ -1,54 +1,24 @@
 class Solution:
     def uniformArray(self, nums1: list[int]) -> bool:
+        nums = sorted(nums1)
 
-        odd = False
-        even = False
+        found_odd = False
+        min_odd = float('inf')
+        min_even = float('inf')
 
-        left_min = [float("inf")]*len(nums1)
-        right_min = [float("inf")]*len(nums1)
-        stack = []
+        for i in range(len(nums)):
+            if nums[i] % 2 == 1:
+                min_odd = nums[i]
+                found_odd = True
+                break
+            else:
+                min_even = min(min_even, nums[i])
 
-        for i in range(len(nums1)-1,-1,-1):
-            while stack and stack[-1]>nums1[i]:
-                stack.pop()
-            if stack:
-                right_min[i] = stack[-1]
-            stack.append(nums1[i])
-            if not odd and  nums1[i] % 2 == 1:
-                odd = True
-            if not even and nums1[i] % 2 == 0 :
-                even = True
-        
-        if not odd or not even :
+        if not found_odd:
             return True
+        
+        
 
-        stack = []
-
-        for i in range(len(nums1)):
-            while stack and stack[-1] > nums1[i] :
-                stack.pop()
-            if stack:
-                left_min[i] = stack[-1]
-            stack.append(nums1[i])
-
-
-        print(left_min)
-        print(right_min)
-
-
-        left = 0
-
-        while left < len(nums1):
-
-            if nums1[left]%2 == 1 :
-                left += 1
-                continue
-
-            elif nums1[left]%2 == 0:
-                if left_min[left] < nums1[left] or right_min[left] < nums1[left]:
-                    left += 1
-                    continue
-                else:
-                    return False
-    
-        return True
+        return min_odd < min_even
+                
+                
